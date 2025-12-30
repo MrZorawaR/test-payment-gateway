328e2ec7d764b44c75b478c7051fa5aad286d87e
MrZorawaR <nitishshyoran420@gmail.com>
2025-12-30 16:28:41 +0530

    testing

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,8 @@
 import { supabase } from "@/lib/supabase";
 
 export default async function Admin() {
-  const { data } = await supabase.from("bookings").select("*");
-
+  const { data } = await supabase.from("bookings").select("*").order("created_at", { ascending: false });
+  console.log("data", data);
   return (
     <div>
       <h1>Admin – Bookings</h1>
